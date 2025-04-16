e077f24843930c85ea198fbf4450fcae65d8863c
Jakub <zuber.kuba@gmail.com>
2025-04-16 17:25:48 +0200

    ref: move chaos filetype autocommand from .config/nvim

--- ftplugin/chaos.lua
+++ ftplugin/chaos.lua
@@ -1,7 +1,24 @@
+local chaos_group = vim.api.nvim_create_augroup("chaos", {})
+
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "chaos",
-	callback = function(event)
+	group = chaos_group,
+	desc = "load chaos highlights",
+	callback = function(_)
 		require'chaos.highlights'
 	end,
-	desc = "Chaos colors for filetype change"
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "load chaos window options",
+	pattern = "chaos",
+	group = chaos_group,
+	callback = function()
+		vim.wo.wrap = true
+		vim.wo.breakindent = true
+		vim.wo.linebreak = true
+		vim.wo.showbreak = ">"
+		vim.wo.breakindentopt = "shift:2"
+	end,
+})
+
